23505ee4c30f6e044719caccf730bfbfe6adfcf1
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-06 01:39:27 -0300

    implementando menu mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,11 @@
-import { Menu } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
 
-import { Button } from '@/components/ui/button'
+import { MobileSidebar } from '@/components/MobileSidebar'
 
 export function Navbar() {
   return (
     <div className="flex items-center p-4">
-      <Button variant={'ghost'} size={'icon'} className="md:hidden">
-        <Menu />
-      </Button>
+      <MobileSidebar />
 
       <section className="flex justify-end w-full">
         <UserButton afterSignOutUrl="/" />
