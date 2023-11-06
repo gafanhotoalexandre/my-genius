eebf7eb46ca1f3b3e0a54eec93993b98c43a60c8
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-06 01:49:21 -0300

    apresentando menu selecionado

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,13 +2,17 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 import { Montserrat } from 'next/font/google'
+
 import { cn } from '@/lib/utils'
 import { sidebarRoutes } from '@/constants'
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] })
 
 export function Sidebar() {
+  const pathname = usePathname()
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-gray-900 text-white">
       <section className="px-3 py-2 flex-1">
@@ -26,7 +30,12 @@ export function Sidebar() {
             <Link
               key={route.href}
               href={route.href}
-              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                pathname === route.href
+                  ? 'text-white bg-white/10'
+                  : 'text-zinc-400',
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
