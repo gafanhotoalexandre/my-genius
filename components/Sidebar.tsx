3bf1494de28c784735a185926e1a6a19339a3283
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-06 16:24:10 -0300

    sizes?

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -18,7 +18,12 @@ export function Sidebar() {
       <section className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative h-8 w-8 mr-4">
-            <Image fill src="/logo.png" alt="logo do Inspire" />
+            <Image
+              fill
+              src="/logo.png"
+              sizes="(max-width: 768px) 100vw, 33vw"
+              alt="logo do Inspire"
+            />
           </div>
           <h1 className={cn('text-2xl font-bold', montserrat.className)}>
             Inspire
