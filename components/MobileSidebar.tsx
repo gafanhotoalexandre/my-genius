5f2dae50a9b2b2a0e24f8328e5987c15c2f5cf92
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-11-06 01:41:49 -0300

    corrigindo erro de hidratacao

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,11 +1,20 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Menu } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Sidebar } from '@/components/Sidebar'
 
 export function MobileSidebar() {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
+
   return (
     <Sheet>
       <SheetTrigger>
